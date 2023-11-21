9119b959cabc4561f4754ed4ee4a853d728a8bd1
GalisGalisteo <galis.travelling@gmail.com>
2023-11-21 18:03:55 +0100

    refactor, adding grid style, board dimensions X-Y, timeOut and endGame

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,109 @@
 <script setup>
-import { ref, computed } from 'vue';
+import { ref } from "vue";
 
 // Dimensiones del tablero
-const dimensions = ref(4)
+const dimensionsX = ref(4);
+const dimensionsY = ref(4);
+
+const board = () => {
+  const tilesArray = [];
+  const totalTiles = dimensionsX.value * dimensionsY.value;
+  // aquí realmente se pondra el array de fotos o símbolos
+  for (let i = 1; i <= totalTiles / 2; i++) {
+    tilesArray.push(i);
+  }
+  for (let i = 1; i <= totalTiles / 2; i++) {
+    tilesArray.push(i);
+  }
+  return tilesArray;
+};
 
 // Valores de las cartas
-const cards = ref([1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8])
+const cards = ref(board());
 
 // Variable que designa si estamos jugando o no
-const isPlaying = ref(false)
+const isPlaying = ref(false);
 
 // Variable que designa si debemos girar una carta o no
 // const isFlipped = ref(false)
 
 // Referencia a la primera carta volteada
-const firstSelectedCard = ref(null)
+const firstSelectedCard = ref(null);
 
 // Referencia a la segunda carta volteada
-const secondSelectedCard = ref(null)
+const secondSelectedCard = ref(null);
 
 // Array con cartas acertadas
 
-const matches = ref([])
+const matches = ref([]);
 
 const newGame = () => {
-  if (dimensions.value % 2 !== 0) {
-        throw new Error("The dimension of the board must be an even number.")
-    }
+  // if (dimensions.value % 2 !== 0) {
+  //   throw new Error("The dimension of the board must be an even number.");
+  // }
   console.log("Game starts");
-  isPlaying.value = true
-}
+  isPlaying.value = true;
+};
+
+let isTimeoutActive = false;
+let firstClick = false;
 
 const checkCards = (card, index) => {
-  
-  
-  if (firstSelectedCard.value === null) {
-    //First click
-    firstSelectedCard.value = index
+  if (isTimeoutActive) {
+    return;
   }
-  else {
-    secondSelectedCard.value = index
-   
+  if (firstClick === false) {
+    firstSelectedCard.value = index;
+    firstClick = true;
+  } else {
+    secondSelectedCard.value = index;
+    firstClick = false;
   }
   if (firstSelectedCard.value !== null && secondSelectedCard.value !== null) {
-    console.log('Ninguno es null');
-    
-    if (cards.value[firstSelectedCard.value] === cards.value[secondSelectedCard.value] ) {
-      console.log('Hago push?');
-      matches.value.push(card)
+    if (
+      cards.value[firstSelectedCard.value] ===
+      cards.value[secondSelectedCard.value]
+    ) {
+      matches.value.push(card);
     }
-   
-    firstSelectedCard.value = null
-    secondSelectedCard.value = null
-    //Falta comprobar si ha acabado el juego
+    isTimeoutActive = true;
+
+    setTimeout(() => {
+      firstSelectedCard.value = null;
+      secondSelectedCard.value = null;
+      isTimeoutActive = false;
+    }, 1000);
+    if (matches.value.length * 2 === cards.value.length) {
+      isPlaying.value = false;
+      matches.value = [];
+    }
   }
-
-}
-
-
+};
 </script>
 
 <template>
   <main>
     <h1>Crazy Tiles</h1>
-    <section v-if="isPlaying"  class="game" >
+    <section
+      v-if="isPlaying"
+      class="game"
+      :style="{ gridTemplateColumns: 'auto '.repeat(dimensionsX) }"
+    >
       <article v-for="(card, index) in cards" :key="index">
-        <button @click="checkCards(card, index)"><span v-if="firstSelectedCard == index || secondSelectedCard == index">{{ card }}</span></button>
+        <button
+          class="tile"
+          @click="checkCards(card, index)"
+          :disabled="matches.includes(card)"
+        >
+          <span
+            v-show="
+              firstSelectedCard == index ||
+              secondSelectedCard == index ||
+              matches.includes(card)
+            "
+            >{{ card }}</span
+          >
+        </button>
       </article>
     </section>
     <section v-else>
@@ -75,19 +113,22 @@ const checkCards = (card, index) => {
 </template>
 
 <style scoped>
-    .game {
-      display: flex;
-      width: 50%;
-    }
-    button { 
-      box-sizing: border-box;
-      width: 50px; 
-      height: 50px; 
-      margin: 0;
-      padding: 0;  
-      border: 1px solid #404040; 
-    }
+.game {
+  display: grid;
+  justify-content: center;
+}
 
-   
-   
+article {
+  width: 50px;
+  height: 50px;
+}
+.tile {
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  border: 1px solid #404040;
+  /* background-image: url(./assets/images/perro.jpeg); */
+}
 </style>
