8110e0a2bcf21c8666a25dd07b5e393ef604e680
gegebe <ggirbes@gmail.com>
2023-11-27 16:02:12 +0100

    estils a medias

--- src/components/ModalConfig.vue
+++ src/components/ModalConfig.vue
@@ -23,7 +23,7 @@ const soSelected = ref("");
             ><img src="../assets/imgs/icon-menu-close.svg" alt=""></button>
         </div>
         <!--Inicio FORM-->
-        <form>
+        <form class="container">
         
         <!--dficultat-->
         <div class="">
@@ -31,15 +31,15 @@ const soSelected = ref("");
             <h2>Dificultat</h2>
               <ul>
                 <li>
-                  <input id="facil" type="radio" value="easy" v-model="difficultySelected">
+                  <input id="facil" type="radio" value="easy" v-model="difficultySelected" name="dificultat">
                   <label for="facil">Fàcil</label>
                 </li>
                 <li>
-                  <input id="mitjana" type="radio" value="medium" v-model="difficultySelected">
+                  <input id="mitjana" type="radio" value="medium" v-model="difficultySelected" name="dificultat">
                   <label for="mitjana">Mitjana</label>
                 </li>
                 <li>
-                  <input id="dificil" type="radio" value="hard" v-model="difficultySelected">
+                  <input id="dificil" type="radio" value="hard" v-model="difficultySelected" name="dificultat">
                   <label for="dificil">Difícil</label>
                 </li>
               </ul>
@@ -52,11 +52,12 @@ const soSelected = ref("");
             <h2>So</h2>
             <ul>
               <li>
-                <input id="on" type="radio" value="on" v-model="soSelected">
+                <span class="custom"></span>
+                <input id="on" type="radio" value="on" v-model="soSelected" name="so">
                 <label for="on">On</label>
               </li>
               <li>
-                <input id="off" type="radio" value="off" v-model="soSelected">
+                <input id="off" type="radio" value="off" v-model="soSelected" name="so">
                 <label for="off">Off</label>
               </li>
               <li>
@@ -71,11 +72,11 @@ const soSelected = ref("");
         <h2>Temàtica</h2>
               <ul>
                 <li>
-                  <input id="numeros" type="radio" value="numbers">
+                  <input id="numeros" type="radio" value="numbers" name="tema">
                   <label for="numeros">Números</label>
                 </li>
                 <li>
-                  <input id="fruites" type="radio" value="fruits">
+                  <input id="fruites" type="radio" value="fruits" name="tema">
                   <label for="fruites">Fruites</label>
                 </li>
               </ul>
@@ -97,8 +98,66 @@ const soSelected = ref("");
   box-sizing: border-box;
 }
 
+/*Les mides tipogràfiques al disseny son molt petites, poder caldria repensar-ho*/
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+  gap:12px;
+  
+  max-width: 330px;
+  width:100%;
+  margin:0 auto;
+
+  font-family: 'Inter', Helvetica, Arial;
+  font-weight: 500;
+  font-size: 14px;
+}
+
+.container h2 {
+  text-align: left;
+  font-size: 24px;
+  font-weight: 600;
+}
+
+ul {
+  display: flex;
+}
+
+/*He estat fent una ullada aquí: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_custom_radio*/
+
 ul li {
   list-style-type:none;
+  display:flex;
+  align-items: center;
+  gap:9px;
+
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
 }
+/*Amaga els type="radio" dels navegadors, no els renderitza*/
+input[type="radio"]{
+  accent-color: var(--config-bg);
+  width: 0px;
+  height: 0px;
+  visibility:collapse;
+}
+/*Aquesta class crea un radio "fals" seguir el disseny*/
+.custom {
+  height: 20px;
+  width: 20px;
+  border-radius: 50%;
+  padding:1px;
+  border:1px solid var(--config-bg);
+  outline-offset: 1px solid var(--config-bg);
+}
+/*Estic intentant que en estat marcat, aparegui la rodona.svg*/
+.custom:checked {
+  background-image: url(../assets/imgs/rodona.svg) center;
+  background-size: cover;
+}
+
 
 </style>
